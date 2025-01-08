9bd9f33ecf7aeb88c2db255a47aa9de5370533c6
mpoeta <mpoeta.dev@gmail.com>
2025-01-08 22:24:00 +0100

    Add logging middleware

--- UserManagementAPI/Program.cs
+++ UserManagementAPI/Program.cs
@@ -1,23 +1,35 @@
-using Microsoft.AspNetCore.Builder;
 using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo 
-    { 
-        Title = "User Management API", 
-        Version = "v1" 
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "User Management API",
+        Version = "v1"
     });
 });
 
 var app = builder.Build();
+
+// Middleware to log HTTP method, request path, and response status code
+app.Use(async (context, next) =>
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    var method = context.Request.Method;
+    var path = context.Request.Path;
+
+    await next.Invoke();
+
+    var statusCode = context.Response.StatusCode;
+    logger.LogInformation("HTTP {Method} {Path} responded {StatusCode}", method, path, statusCode);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
